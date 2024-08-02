a6fccfa42b01c502e4a4dc2b2f5145eae8785bd2
Vamsi Krishna <vamsi.forwork@gmail.com>
2024-08-02 14:46:29 +0530

    Todos List App

--- app.js
+++ app.js
@@ -1,8 +1,39 @@
 var express = require('express');
 var app = express();
+const path = require('path')
+
+// Serve static files from the 'frontend' directory
+app.use(express.static('frontend'));
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  // res.send('Hello World!');
+  res.sendFile(__dirname + '/frontend/js/app.js'); // Ensure the path is correct
+
 });
+  // // Route for the signup page
+  // app.get('/signup', function(req, res){
+  //   res.sendFile(__dirname, '/frontend/js/app.js');
+  // })
+
+  // // Route for the login page
+  // app.get('/login', function(req, res){
+  //   res.sendFile(__dirname, '/frontend/js/app.js');
+  // })  
+// });
+
+// // Serve static files from the "frontend" directory
+// app.use(express.static(path.join(__dirname, 'frontend/js/app.js')));
+
+// // Route for the home page
+// app.get('/login', (req, res) => {
+//     res.sendFile(path.join(__dirname, '/frontend/js/app.js'));
+// });
+
+// // Handle all other routes for SPA
+// app.get('/signup', (req, res) => {
+//     res.sendFile(path.join(__dirname, '/frontend/js/app.js'));
+// });
+
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log(`Example app listening on port http://localhost:${3000}`);
 });
